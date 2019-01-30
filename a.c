ca79f71d0d646a4564163ce1314e23a2c0e8e03a
Xiguo Xie <xiexiguo@live.com>
2019-01-30 16:14:07 +0800

    modify a.c

--- a.c
+++ a.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
+int add(int, int);
+
 int main(int argc, char** argv)
 {
     int a;
     int b;
-    add(a,b);
+    int c;
+    c=add(a,b);
     printf("a=%d\n",a);
+    printf("b=%d\n",b);
+    printf("c=%d\n",c);
+    
+    return 0;
 }
